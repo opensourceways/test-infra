f88f43837e6d83239acbcaeb368d6d3205d0927f
xwzQmxx <1499273991@qq.com>
2021-03-24 17:23:39 +0800

    fix bug

--- prow/job-reporter/gitee/client.go
+++ prow/job-reporter/gitee/client.go
@@ -115,12 +115,19 @@ func (c *ghclient) CreateStatus(org, repo, ref string, s github.Status) error {
 
 	uErr := c.updatePRLabel(org, repo, int32(prNumber), pr.Labels, status)
 	// oldSha == "" means there is not status comment exist.
+	tpl := "report job status label or comment error, label error: %v; comment error: %v"
 	if oldSha == "" {
 		err = c.CreatePRComment(org, repo, prNumber, desc)
-		return fmt.Errorf("report job status label or comment error, label error: %v; comment error: %v", uErr, err)
+		if uErr != nil || err != nil {
+			return fmt.Errorf(tpl, uErr, err)
+		}
+		return nil
 	}
 	err = c.UpdatePRComment(org, repo, commentID, desc)
-	return fmt.Errorf("report job status label or comment error, label error: %v; comment error: %v", uErr, err)
+	if uErr != nil || err != nil {
+		return fmt.Errorf(tpl, uErr, err)
+	}
+	return nil
 }
 
 func (c *ghclient) updatePRLabel(org, repo string, number int32, labels []sdk.Label, status []github.Status) error {
